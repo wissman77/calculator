ec5fababe11944947877f5dc6375d98a30ead3e2
Wissam Mansour <wissman77@gmail.com>
2022-11-21 00:04:53 +0200

    Add operate function

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const divide = (a, b) => {
   return (a / b).toFixed(3);
 };
 
+// operate function get an operator and 2 numbers and returns the result of the operation
+const operate = (operator, firstNum, secondNum) => {
+  switch (operator) {
+    case '+':
+      return add(firstNum, secondNum);
+      break;
+    case '-':
+       return subtract(firstNum, secondNum);
+      break;
+    case '/':
+      return divide(firstNum, secondNum);
+      break;
+    case '*':
+      return multiply(firstNum, secondNum);
+      break
+    default:
+      return 'Unknown Operator';
+      break;
+  }
+};
