a1e0b846aea5eb1b71a1d17aa6ed8b908df8dbb3
Wissam Mansour <wissman77@gmail.com>
2022-12-03 00:51:39 +0200

    Select All DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// elements selection
+const lastScreen = document.querySelector('.screen-last');
+const currentScreen = document.querySelector('.screen-last');
+const clearButton = document.querySelector('.btn-clear');
+const deleteButton = document.querySelector('.btn-delete');
+const digitButtons = document.querySelectorAll('[data-digit]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalButton = document.querySelector('[data-equal]');
+const pointButton = document.querySelector('[data-point]')
+
+
 // basic math functions
 const add = (a, b) => (a + b).toFixed(3);
 const subtract = (a, b) => (a - b).toFixed(3);
