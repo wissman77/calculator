1f05bd698aaaaabe93656cadc8cd7e8afc6e629a
Wissam Mansour <wissman77@gmail.com>
2022-12-03 06:48:22 +0200

    Fix typo

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => {
-  if (b === 0) return 'Cannot divide on zero';
+  if (b === 0) return 'Cannot divide by zero';
   return a / b;
 };
 
